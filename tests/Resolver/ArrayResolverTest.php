6a3f019deff39848211c519279db667cb5cfee7d
Dries De Peuter <dries@stenic.io>
2026-04-15 22:28:45 +0200

    fix: Implement remove
    fixes #100

--- tests/Resolver/ArrayResolverTest.php
+++ tests/Resolver/ArrayResolverTest.php
@@ -93,4 +93,69 @@ class ArrayResolverTest extends TestCase
 
         $this->assertEquals([$job], $property->getValue($this->resolver));
     }
+
+    public function testRemoveJob()
+    {
+        $property = new \ReflectionProperty($this->resolver, 'jobs');
+        $property->setAccessible(true);
+
+        $job1 = new ShellJob();
+        $job2 = new ShellJob();
+        $this->resolver->addJob($job1);
+        $this->resolver->addJob($job2);
+
+        $this->resolver->removeJob($job1);
+
+        $this->assertEquals([$job2], $property->getValue($this->resolver));
+    }
+
+    public function testRemoveJobNotInList()
+    {
+        $property = new \ReflectionProperty($this->resolver, 'jobs');
+        $property->setAccessible(true);
+
+        $job1 = new ShellJob();
+        $job2 = new ShellJob();
+        $this->resolver->addJob($job1);
+
+        $this->resolver->removeJob($job2);
+
+        $this->assertEquals([$job1], $property->getValue($this->resolver));
+    }
+
+    public function testRemoveJobs()
+    {
+        $property = new \ReflectionProperty($this->resolver, 'jobs');
+        $property->setAccessible(true);
+
+        $job1 = new ShellJob();
+        $job2 = new ShellJob();
+        $job3 = new ShellJob();
+        $this->resolver->addJob($job1);
+        $this->resolver->addJob($job2);
+        $this->resolver->addJob($job3);
+
+        $this->resolver->removeJobs([$job1, $job3]);
+
+        $this->assertEquals([$job2], $property->getValue($this->resolver));
+    }
+
+    public function testRemoveJobReindexesArray()
+    {
+        $property = new \ReflectionProperty($this->resolver, 'jobs');
+        $property->setAccessible(true);
+
+        $job1 = new ShellJob();
+        $job2 = new ShellJob();
+        $job3 = new ShellJob();
+        $this->resolver->addJob($job1);
+        $this->resolver->addJob($job2);
+        $this->resolver->addJob($job3);
+
+        $this->resolver->removeJob($job2);
+
+        $jobs = $property->getValue($this->resolver);
+        $this->assertEquals([$job1, $job3], $jobs);
+        $this->assertSame([0, 1], array_keys($jobs));
+    }
 }
